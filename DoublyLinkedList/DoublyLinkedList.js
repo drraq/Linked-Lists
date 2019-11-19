5dc921a3fc001758d850b42306e6db600d40b1fe
drraq <diracist@gmail.com>
2019-11-19 18:51:08 +0500

    Doubly Linked List Push Method Added

--- DoublyLinkedList/DoublyLinkedList.js
+++ DoublyLinkedList/DoublyLinkedList.js
@@ -6,7 +6,21 @@ class DoublyLinkedList {
     this.tail = null;
     this.size = 0;
   }
+  push(data) {
+    var node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      var last = this.tail;
+      this.tail = node;
+      this.tail.prev = last;
+      last.next = this.tail;
+    }
+    this.size++;
 
+    return this;
+  }
 }
 
 module.exports = DoublyLinkedList;
---
+++ DoublyLinkedList/main.js
@@ -0,0 +1,9 @@
+const DoublyLinkedList = require('./DoublyLinkedList.js');
+
+var ll = new DoublyLinkedList();
+// dLL.push('Rehan');
+// dLL.push('Abdul');
+// dLL.push('Qadir');
+// dLL.push('Mohsin');
+ll.push(200).push(300).push(400);
+console.log(ll);
